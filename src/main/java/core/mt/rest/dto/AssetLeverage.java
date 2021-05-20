957bb9d1179495da2ac4f0f04d62db372884bdbb
abmetko.a <abmetko.a@gmail.com>
2021-05-20 23:03:42 +0300

    refactored AssetLeverage.java

--- src/main/java/core/mt/rest/dto/AssetLeverage.java
+++ src/main/java/core/mt/rest/dto/AssetLeverage.java
@@ -1,48 +1,64 @@
 package core.mt.rest.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.*;
 
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "symbol",
+        "leverage",
+        "margin"
+})
 public class AssetLeverage {
 
-    @JsonProperty(value = "symbol")
+    @JsonProperty("symbol")
     private String symbol;
-
-    @JsonProperty(value = "leverage")
+    @JsonProperty("leverage")
     private Double leverage;
-
-    @JsonProperty(value = "margin")
+    @JsonProperty("margin")
     private Double margin;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-    public void setSymbol(String symbol){
-        this.symbol = symbol;
-    }
-
-    public void setLeverage(Double leverage){
-        this.leverage = leverage;
-    }
-
-    public String getSymbol(){
+    @JsonProperty("symbol")
+    public String getSymbol() {
         return symbol;
     }
 
-    public Double getLeverage(){
+    @JsonProperty("symbol")
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @JsonProperty("leverage")
+    public Double getLeverage() {
         return leverage;
     }
 
+    @JsonProperty("leverage")
+    public void setLeverage(Double leverage) {
+        this.leverage = leverage;
+    }
+
+    @JsonProperty("margin")
     public Double getMargin() {
         return margin;
     }
 
+    @JsonProperty("margin")
     public void setMargin(Double margin) {
         this.margin = margin;
     }
-}
 
-/*
-{
-    "symbol": "ADAUSD",
-    "leverage": 2.0,
-    "margin": 89.0
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
 }
-*/
