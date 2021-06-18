2481e1d4448ba61a29393d0b4b93ff95327916eb
abmetko.a <abmetko.a@gmail.com>
2021-06-18 11:45:49 +0300

    implemented Retry.java

---
+++ src/main/java/core/mt/utils/Retry.java
@@ -0,0 +1,27 @@
+package core.mt.utils;
+
+import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
+
+
+public class Retry {
+
+    private static void sleep(int sleepTime) {
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void whileTrue(int pollingIntervalMs, int timeoutMilliseconds, Supplier<Boolean> supplier, String exceptionMsg) throws TimeoutException {
+        long timeStart = System.currentTimeMillis();
+        while (supplier.get()) {
+            System.out.println("it's true");
+            if (System.currentTimeMillis() - timeStart > timeoutMilliseconds) {
+                throw new TimeoutException(String.format("Time out after %d ms: ", timeoutMilliseconds) + exceptionMsg);
+            }
+            sleep(pollingIntervalMs);
+        }
+    }
+}
